418b997fab4133d4fddcbd7dcee8beffdec4785f
Odd Andre Owren <oddandreowren@gmail.com>
2021-01-18 12:56:57 +0100

    begin class with draw-functions#

--- project1/game/board_visualizer.py
+++ project1/game/board_visualizer.py
@@ -2,19 +2,19 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from board import Board
 
-board = Board()
+class BoardVisualizer:
 
-G=nx.Graph()
-for i in range(len(board.board)):
-    for n in range(len(board.board[i])):
-        elem = board.board[i][n]
-        G.add_node(i*(len(board.board))+n+1)
-        """if elem == 1:
-            G.add_node((n+1)*(i+1))
-        elif elem == 2:
-            G.add_node(n)
-        else: 
-            G.add_node(n)"""
+    def __init__(self, display_episode, delay, open_cells=[(2,2)]):
+        # Extend to create different boards
+        self.display_episode = display_episode
+        self.delay = delay
+        self.board = Board(open_cells=self.open_cells)
+        self.graph = nx.Graph()
 
-nx.draw(G)
-plt.show()
+    def draw_board(self):
+        self.graph.add_nodes_from(self.open_cells, color = "black")
+        nx.draw(self.graph)
+        plt.show()
+
+    def get_empty_positions(self):
+
