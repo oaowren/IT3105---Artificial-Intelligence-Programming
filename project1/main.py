3d515e2908bd241ced77ae2cb3cfaa0f13e97dab
Erling Van De Weijer <erling.weijer@gmail.com>
2021-01-18 19:55:57 +0000

    Add visualization of boards, for both diamond and triangle shape

--- project1/main.py
+++ project1/main.py
@@ -1,10 +1,17 @@
 from game.board import Board
+from game.board_visualizer import BoardVisualizer
+import time
 
 
 if __name__ == "__main__":
-    board = Board(board_type="T", board_size=5, open_cells=[(2,2)])
+    board = Board(board_type="D", board_size=7, open_cells=[(2,2), (1,1)])
+    boardVisualizer = BoardVisualizer(width=1000, height=800)
+    boardVisualizer.draw_board(board.board, board.board_type)
+    time.sleep(5) #Sleep to display the board for some time
     board.print_board()
     board.make_move((4, 0), (2, 2))
+    boardVisualizer.draw_board(board.board, board.board_type)
+    time.sleep(5) #Sleep to display the board for some time
     board.print_board()
     board.make_move((3, 0), (1, 2))
     board.print_board()
