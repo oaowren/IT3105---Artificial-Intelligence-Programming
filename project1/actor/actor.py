f15b07cd09b2dbe09b4ebf5482f680d6014d5dcd
Odd Andre Owren <oddandreowren@gmail.com>
2021-01-18 13:24:12 +0100

    create initial classes for actor and critic with relevant parameters

---
+++ project1/actor/actor.py
@@ -0,0 +1,4 @@
+class Actor():
+
+    def __init__(self, lr=0.5, eligibility_decay=0, discount_factor=0.5, initial_epsilon=1, epsilon_decay_rate=0.1):
+        
---
+++ project1/critic/critic.py
@@ -0,0 +1,4 @@
+class Critic():
+
+    def __init__(self, method="TL", nn_dimensions=None, lr=0.5, eligibility_decay=0, discount_factor=0.5):
+        
