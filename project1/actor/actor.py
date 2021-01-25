cf4561211820d9e55324b1ae7863bfcc81d536b0
Odd Andre Owren <oddandreowren@gmail.com>
2021-01-25 12:14:53 +0100

    simple actor with no policy and epsilon-greedy started

--- project1/actor/actor.py
+++ project1/actor/actor.py
@@ -1,4 +1,26 @@
+import random
+
 class Actor():
 
-    def __init__(self, lr=0.5, eligibility_decay=0, discount_factor=0.5, initial_epsilon=1, epsilon_decay_rate=0.1):
-        
+    def __init__(self, board, lr=0.5, eligibility_decay=0, discount_factor=0.5, initial_epsilon=0.1, epsilon_decay_rate=0.1):
+        self.board = board
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay_rate
+        self.policy = {}
+
+    def select_action(self):
+        moves = self.board.get_all_legal_moves()
+        random_choice = random.random()
+        if random_choice < self.epsilon:
+            return random.choice(moves)
+        else:
+            best_move = moves[0]
+            for move in moves:
+                try: 
+                    try_move = self.policy[(self.board, move)]
+                    if try_move > best_move:
+                        best_move = try_move
+                except KeyError:
+                    pass
+            return best_move
+
--- project1/main.py
+++ project1/main.py
@@ -1,17 +1,42 @@
 from game.board import Board
 from game.board_visualizer import BoardVisualizer
+from actor.actor import Actor
 import time
 
 
+# ------ VARIABLES --------
+# Board and Game Variables
+board_type = "D"                # "T" or "D"
+board_size = 4
+open_cells = [(2,2), (1,1)]
+number_of_episodes = 25
+display_episode = number_of_episodes - 1    # Display final run
+display_delay = 2               # Number of seconds between board updates in visualization
+
+# Critic Variables
+critic_method = "TL"            # "TL" or "NN"
+critic_nn_dims = (15, 20, 30, 5, 1) 
+lr_critic = 0.1
+eligibility_decay_critic = 0.1
+discount_factor_critic = 0.1
+# Actor Variables
+lr_actor = 0.1
+eligibility_decay_actor = 0.1
+discount_factor_actor = 0.1
+epsilon = 0.1
+epsilon_decay = 0.1
+
+# Main-function for running everything
 if __name__ == "__main__":
-    board = Board(board_type="D", board_size=7, open_cells=[(2,2), (1,1)])
+    board = Board(board_type=board_type, board_size=board_size, open_cells=open_cells)
     boardVisualizer = BoardVisualizer(width=1000, height=800)
+    actor = Actor(board)
     boardVisualizer.draw_board(board.board, board.board_type)
-    time.sleep(5) #Sleep to display the board for some time
-    board.print_board()
-    board.make_move((4, 0), (2, 2))
-    boardVisualizer.draw_board(board.board, board.board_type)
-    time.sleep(5) #Sleep to display the board for some time
-    board.print_board()
-    board.make_move((3, 0), (1, 2))
-    board.print_board()
+    time.sleep(display_delay) #Sleep to display the board for some time
+    while not board.check_losing_state() and not board.check_winning_state():
+        board.print_board()
+        selected_move = actor.select_action()
+        board.make_move(selected_move)
+        boardVisualizer.draw_board(board.board, board.board_type)
+        time.sleep(display_delay) #Sleep to display the board for some time
+
