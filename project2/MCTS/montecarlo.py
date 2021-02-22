73af207a07b815c948ab1809f98cc3717e704d47
Odd Andre Owren <oddandreowren@gmail.com>
2021-02-22 11:49:02 +0100

    ini MCTS, add required parameters

---
+++ project2/MCTS/montecarlo.py
@@ -0,0 +1,6 @@
+class MCTS():
+
+    def __init__(self, root):
+        self.root = root
+        self.tree_policy = {}
+        pass
--- project2/parameters.py
+++ project2/parameters.py
@@ -1,7 +1,18 @@
 class Parameters():
 
     def __init__(self):
+        # Board parameters
+        self.board_size = 4
+        # MCTS parameters
+        self.number_of_episodes = 100
+        self.number_of_search_episodes = 1000
+        # ANET parameters
         self.lr = 0.001
         self.nn_dims = (15,25,20,5)
+        self.activation_function = "sigmoid"
+        self.optimizer = "adam"
+        # TOPP parameters
+        self.number_of_cached_anet = 100
+        self.number_of_games = 100
 
     
