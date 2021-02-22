c481c237f9b9b130caa674e943c6e8e6b0c040e3
Odd Andre Owren <oddandreowren@gmail.com>
2021-02-22 12:05:36 +0100

    update neuralnet to make use of parameters

--- project2/NeuralNetwork/neuralnet.py
+++ project2/NeuralNetwork/neuralnet.py
@@ -2,19 +2,42 @@ from tensorflow import keras as ks
 
 class NeuralNet():
 
-    def __init__(self, nn_dims, board_size, lr):
-        self.model = self.init_model(nn_dims, board_size, lr)
+    def __init__(self, nn_dims, board_size, lr, activation, optimizer):
+        self.model = self.init_model(nn_dims, board_size, lr, activation, optimizer)
 
-    def init_model(self, nn_dims, board_size, lr):
+    def init_model(self, nn_dims, board_size, lr, activation, optimizer):
         model = ks.Sequential()
+        activation_function = self.select_activation(activation)
+        if activation_function is None:
+            raise ValueError("Invalid activation function provided (must be either 'linear', 'sigmoid', 'tanh' or 'relu')")
         model.add(ks.Input(shape=(board_size**2, )))
-        model.add(ks.layers.Dense(board_size**2))
+        model.add(ks.layers.Dense(board_size**2, activation=activation_function))
         for i in nn_dims:
-            model.add(ks.layers.Dense(i))
-        model.add(ks.layers.Dense(board_size**2))
-        model.add(ks.layers.Activation("softmax"))
-        model.compile(optimizer=ks.optimizers.Adam(learning_rate=lr),
+            model.add(ks.layers.Dense(i, activation=activation_function))
+        model.add(ks.layers.Dense(board_size**2, activation="softmax"))
+        o = self.select_optimizer(optimizer)
+        if o is None:
+            raise ValueError("Invalid optimizer provided (must be either 'adagrad', 'sgd', 'rmsprop' or 'adam')")
+        model.compile(optimizer=o(learning_rate=lr),
             loss='categorical_crossentropy',
             metrics=["accuracy"])
         model.summary()
         return model
+
+    def select_activation(self, activation_function):
+        acts = {
+            "linear": ks.activations.linear,
+            "sigmoid": ks.activations.sigmoid,
+            "tanh": ks.activations.tanh,
+            "relu": ks.activations.relu
+        }
+        return acts.get(activation_function, None)
+
+    def select_optimizer(self, optimizer):
+        optimizers = {
+            "adagrad": ks.optimizers.Adagrad,
+            "sgd": ks.optimizers.SGD,
+            "rmsprop": ks.optimizers.RMSprop,
+            "adam": ks.optimizers.Adam
+        }
+        return optimizers.get(optimizer, None)
--- project2/main.py
+++ project2/main.py
@@ -1,5 +1,7 @@
 from NeuralNetwork.neuralnet import NeuralNet
+from parameters import Parameters
+
+p = Parameters()
 
 if __name__ == "__main__":
-    nn = NeuralNet((15,20,10), 3, 0.001)
-    
+    nn = NeuralNet(p.nn_dims, p.board_size, p.lr, p.activation_function, p.optimizer)
