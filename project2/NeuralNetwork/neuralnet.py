7d3dfe6c4becf0197dcbd440e27a94f025d6230d
Odd Andre Owren <oddandreowren@gmail.com>
2021-03-01 15:06:06 +0100

    refactor neuralnet, test with bigger board

--- project2/NeuralNetwork/neuralnet.py
+++ project2/NeuralNetwork/neuralnet.py
@@ -6,7 +6,7 @@ acts = {
     "linear": ks.activations.linear,
     "sigmoid": ks.activations.sigmoid,
     "tanh": ks.activations.tanh,
-    "relu": ks.activations.relu
+    "relu": ks.activations.relu,
 }
 
 # Static values used to select optimizer
@@ -14,58 +14,86 @@ optimizers = {
     "adagrad": ks.optimizers.Adagrad,
     "sgd": ks.optimizers.SGD,
     "rmsprop": ks.optimizers.RMSprop,
-    "adam": ks.optimizers.Adam
+    "adam": ks.optimizers.Adam,
 }
 
-class NeuralNet():
-    def __init__(self, nn_dims, board_size, lr, activation, optimizer, load_saved_model=False, model_name="", episode_number=0):
+
+class NeuralNet:
+    def __init__(
+        self,
+        nn_dims,
+        board_size,
+        lr,
+        activation,
+        optimizer,
+        load_saved_model=False,
+        model_name="",
+        episode_number=0,
+    ):
         if load_saved_model:
             try:
                 self.model = self.load_saved_model(model_name, episode_number)
             except OSError:
-                raise ValueError("Failed to load model named {0}{1}, did you provide correct model name and episode number?".format(model_name, episode_number))
-        else: 
+                raise ValueError(
+                    "Failed to load model named {0}{1}, did you provide correct model name and episode number?".format(
+                        model_name, episode_number
+                    )
+                )
+        else:
             self.model = self.init_model(nn_dims, board_size, lr, activation, optimizer)
 
     def init_model(self, nn_dims, board_size, lr, activation, optimizer):
         model = ks.Sequential()
-        activation_function = self.select_activation(activation)
+        activation_function = acts.get(activation, None)
         if activation_function is None:
-            raise ValueError("Invalid activation function provided (must be either '{0}')".format("', '".join(acts.keys())))
-        model.add(ks.Input(shape=(board_size**2)))
+            raise ValueError(
+                "Invalid activation function provided (must be either '{0}')".format(
+                    "', '".join(acts.keys())
+                )
+            )
+        model.add(ks.Input(shape=(board_size ** 2)))
         # Ensure that input has correct shape
-        model.add(ks.layers.Dense(board_size**2, activation=activation_function))
+        model.add(ks.layers.Dense(board_size ** 2, activation=activation_function))
         for i in nn_dims:
             model.add(ks.layers.Dense(i, activation=activation_function))
         # Ensure that output has correct shape and activation softmax
-        model.add(ks.layers.Dense(board_size**2, activation="softmax"))
-        o = self.select_optimizer(optimizer)
-        if o is None:
-            raise ValueError("Invalid optimizer provided (must be either '{0}')".format("', '".join(optimizers.keys())))
-        model.compile(optimizer=o(learning_rate=lr),
-            loss='categorical_crossentropy',
-            metrics=["accuracy"])
+        model.add(ks.layers.Dense(board_size ** 2, activation="softmax"))
+        opt = optimizers.get(optimizer, None)
+        if opt is None:
+            raise ValueError(
+                "Invalid optimizer provided (must be either '{0}')".format(
+                    "', '".join(optimizers.keys())
+                )
+            )
+        model.compile(
+            optimizer=opt(learning_rate=lr),
+            loss="categorical_crossentropy",
+            metrics=["accuracy"],
+        )
         model.summary()
         return model
-
-    def select_activation(self, activation_function):
-        return acts.get(activation_function, None)
-
-    def select_optimizer(self, optimizer):
-        return optimizers.get(optimizer, None)
 
     def fit(self, inputs, targets, batch_size=None, epochs=1, verbosity=1):
         train_x, train_y, valid_x, valid_y = self.train_test_split(inputs, targets)
         train_x, train_y = self.random_minibatch(train_x, train_y, len(train_x))
-        self.model.fit(train_x, train_y, epochs=epochs, verbose=verbosity)
+        self.model.fit(train_x, train_y, epochs=epochs, verbose=verbosity, batch_size=batch_size)
         self.model.evaluate(valid_x, valid_y, verbose=verbosity)
-            
 
     def predict(self, inputs, flat_board):
         predictions = self.model.predict(inputs)
         pred_length = len(predictions)
-        illegal_moves_removed = np.array([[predictions[n][i] if flat_board[i]==0 else 0 for i in range(len(predictions[n]))] for n in range(pred_length)])
-        illegal_moves_summed = [sum(illegal_moves_removed[i]) for i in range(pred_length)]
+        illegal_moves_removed = np.array(
+            [
+                [
+                    predictions[n][i] if flat_board[i] == 0 else 0
+                    for i in range(len(predictions[n]))
+                ]
+                for n in range(pred_length)
+            ]
+        )
+        illegal_moves_summed = [
+            sum(illegal_moves_removed[i]) for i in range(pred_length)
+        ]
         return illegal_moves_removed / illegal_moves_summed
 
     def save_model(self, model_name, episode_number):
@@ -73,7 +101,9 @@ class NeuralNet():
         print("Model {0}{1} saved succesfully".format(model_name, episode_number))
 
     def load_saved_model(self, model_name, episode_number):
-        model = ks.models.load_model("project2/models/{0}{1}.h5".format(model_name, episode_number))
+        model = ks.models.load_model(
+            "project2/models/{0}{1}.h5".format(model_name, episode_number)
+        )
         print("Model {0}{1} loaded succesfully".format(model_name, episode_number))
         return model
 
@@ -84,10 +114,16 @@ class NeuralNet():
     def train_test_split(self, inputs, targets, split=0.1, randomize=True):
         vc = round(split * len(inputs))
         if split > 0:
-            pairs = list(zip(inputs,targets))
-            if randomize: np.random.shuffle(pairs)
-            vcases = pairs[0:vc]; tcases = pairs[vc:]
-            return np.array([tc[0] for tc in tcases]), np.array([tc[1] for tc in tcases]),\
-                   np.array([vc[0] for vc in vcases]), np.array([vc[1] for vc in vcases])
+            pairs = list(zip(inputs, targets))
+            if randomize:
+                np.random.shuffle(pairs)
+            vcases = pairs[0:vc]
+            tcases = pairs[vc:]
+            return (
+                np.array([tc[0] for tc in tcases]),
+                np.array([tc[1] for tc in tcases]),
+                np.array([vc[0] for vc in vcases]),
+                np.array([vc[1] for vc in vcases]),
+            )
         else:
-            return inputs,targets,[],[]
+            return inputs, targets, [], []
--- project2/parameters.py
+++ project2/parameters.py
@@ -2,13 +2,13 @@ class Parameters():
 
     def __init__(self):
         # Board parameters
-        self.board_size = 4
+        self.board_size = 10
         # MCTS parameters
         self.number_of_episodes = 100
         self.number_of_search_episodes = 1000
         # ANET parameters
         self.lr = 0.001
-        self.nn_dims = (15,25,20,5)
+        self.nn_dims = (15, 15, 20, 25, 30, 25, 20, 15)
         self.activation_function = "sigmoid"
         self.optimizer = "adam"
         # TOPP parameters
