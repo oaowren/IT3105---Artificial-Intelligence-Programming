6f9777587ee125cdb20e696d1143c3931c234eb8
Odd Andre Owren <oddandreowren@gmail.com>
2021-03-01 14:22:11 +0100

    add comments explaining the algorithm, supplementing with currently existing methods

--- project2/main.py
+++ project2/main.py
@@ -14,7 +14,36 @@ nn = NeuralNet(p.nn_dims, p.board_size, p.lr, p.activation_function, p.optimizer
 board = Board(p.board_size)
 board_visualizer = BoardVisualizer()
 
+def run_search_game():
+    pass
+    # Use tree policy to search from root to a leaf, update MC-board with each move
+    # argmax(nn.predict(MC-board.state)) to select rollout actions until a final state F, update MC-board with each move
+    # MCTS backprop from F to root
+
+def run_full_game():
+    board.reset_board()
+    # Initialize MCT to root in same state as board_starting_state
+    while not board.check_winning_state:
+        # Initialize Monte Carlo game board (copy of board) to same state as root
+        for search_game in range(p.number_of_search_episodes):
+            run_search_game()
+        # Save distribution D of visit counts in MCT
+        # rbuf.append(root, D)
+        # select move a based on D
+        # Perform a moving board to new state
+        # MCT: retain subtree rooted at new state, discard everything else
+        # root = new state
+    # nn.fit([r[0] for r in rbuf], [r[1] for r in rbuf]) -> produces minibatches when calling fit  
+
+
 if __name__ == "__main__":
+    """
+    for game in range(p.number_of_games):
+        run_full_game()
+        if game % save_interval == 0:
+            nn.save_model("model", game)
+    """
+
     board.board[0][3] = 1
     board.board[2][0] = 2
     flat_board = board.flatten_board()
@@ -36,4 +65,3 @@ if __name__ == "__main__":
     # print(board.check_winning_state())
     # board_visualizer.draw_board(board.board)
     # time.sleep(6)
-
