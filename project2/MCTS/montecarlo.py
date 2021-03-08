8618409cc1e3f45c11036c94c1a3eb625b1760b7
Erling Van De Weijer <erling.weijer@gmail.com>
2021-03-08 13:18:57 +0000

    Add some initial functions to the MCTS

--- project2/MCTS/montecarlo.py
+++ project2/MCTS/montecarlo.py
@@ -1,5 +1,43 @@
+
+"""
+Dette er hovedsaklig basert på algoritmen fra
+http://www.cs.cornell.edu/courses/cs6700/2016sp/lectures/CS6700-UCT.pdf
+side 5-7.
+"""
+
+import numpy as np
+
 class MCTS:
+
     def __init__(self, root):
         self.root = root
         self.tree_policy = {}
+        self.states= {}
+        self.state_action = {}
+        self.c = 1
         pass
+
+    def update(self, state, action, reward):
+        self.state[state]["N"] +=1
+        self.state_action_pair[(state,action)]["N"] +=1
+        self.state_action_pair[(state,action)]["Q"] += (reward - self.get_Q(state, action))/self.get_N(state, action)
+        return
+
+    def get_N(self, state, action=None):
+        if action:
+            if (state,action) not in self.state_action_pairs:
+                self.state_action_pair[(state,action)]["N"] = 0
+            return self.state_action_pair[(state,action)]["N"]
+        if state not in self.state:
+            self.state[state]["N"] = 0
+        return self.state[state]["N"]
+
+    def get_Q(self, state, action):
+        return self.state_Action_pair[(state,action)]["Q"]
+
+    def exploration_bonus(self, state, action):
+        exploration_bonus =self.c*np.sqrt(np.log(self.get_N(state))/self.get_N(state,action))
+
+
+
+
